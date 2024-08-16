991148c9d3e1adfb05adbd7f6d36d26e596d98e9
Amy Lee <amy.jeeyoon.lee@gmail.com>
2024-08-15 21:47:02 -0700

    changed link

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 'use client'
 
-import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Container, Toolbar, Typography, Button, AppBar, Box, Grid  } from "@mui/material";
@@ -10,7 +9,7 @@ export default function Home() {
   const handleSubmit = async () => {
     const checkoutSession = await fetch('/api/checkout_sessions', {
       method: 'POST', 
-      headers: {origin: "http://localhost:3000",} //fix later when deployed
+      headers: {origin: "https://flashcard-saas-five.vercel.app/",} //fix later when deployed
     })
 
     const checkoutSessionJson = await checkoutSession.json();
